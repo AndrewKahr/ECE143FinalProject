30b6a5ff71eb4cdc9ac080c8641821988de05f7c
evanJ98 <yufanj@uci.edu>
2021-12-02 19:22:26 -0800

    add docstring and asserts

--- util.py
+++ util.py
@@ -1,7 +1,16 @@
+from logging import StringTemplateStyle
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 def line_graph(data, title):
+    """[Plot line graph of the data over the year]
+
+    Args:
+        data ([list]): [list of graphing data for each year]
+        title ([string]): [title of the graph]
+    """    
+    assert isinstance(data, list)
+    assert isinstance(title, str)
     fig, ax = plt.subplots(figsize=(10,5))
     ax.set_ylim([0,100])
     ax.set_xlabel("Year")
@@ -10,9 +19,22 @@ def line_graph(data, title):
     ax.plot(np.arange(5),data, 'go-')
     plt.xticks(np.arange(5), ['2016','2017','2018','2019','2020'])
     sns.despine()
-    #plt.savefig('1.png', transparent=True)
 
 def line_graph_double(data1, data2, legend1, legend2, title):
+    """[Plot line graph of two sets of data over the year]
+
+    Args:
+        data1 ([list]): [list of graphing data for each year]
+        data2 ([list]): [list of graphing data for each year]
+        legend1 ([string]): [legend name of data 1]
+        legend2 ([string]): [legend name of data 2]
+        title ([string]): [title of the graph]
+    """    
+    assert isinstance(data1, list)
+    assert isinstance(title, str)
+    assert isinstance(data2, list)
+    assert isinstance(legend1, str)
+    assert isinstance(legend2, str)
     fig, ax = plt.subplots(figsize=(10,5))
     ax.set_ylim([0,100])
     ax.set_xlabel("Year")
@@ -24,4 +46,3 @@ def line_graph_double(data1, data2, legend1, legend2, title):
     plt.xticks(np.arange(5),['2016','2017','2018','2019','2020'])
     ax.legend()
     sns.despine()
-    #plt.savefig('2.png', transparent=True)
